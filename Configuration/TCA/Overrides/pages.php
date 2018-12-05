5985e1bc744a5a989c434ab3c088c7dacd4b7106
A. wolo Wolski <wolo.wolski@gmail.com>
2018-12-05 02:42:46 +0100

    TYPO3 8.7 TCA compatibility fixes

--- Configuration/TCA/Overrides/pages.php
+++ Configuration/TCA/Overrides/pages.php
@@ -3,3 +3,9 @@
 if (is_array($GLOBALS['TCA']['pages']['ctrl']['typeicon_classes'])) {
     $GLOBALS['TCA']['pages']['ctrl']['typeicon_classes']['contains-irfaq'] = 'tcarecords-pages-contains-irfaq';
 }
+
+// Adding sysfolder icon
+$GLOBALS['TCA']['pages']['columns']['module']['config']['items'][$_EXTKEY]['0']
+        = 'LLL:EXT:irfaq/Resources/Private/Language/locallang_db.xlf:tx_irfaq.sysfolder';
+$GLOBALS['TCA']['pages']['columns']['module']['config']['items'][$_EXTKEY]['1'] = $_EXTKEY;
+
